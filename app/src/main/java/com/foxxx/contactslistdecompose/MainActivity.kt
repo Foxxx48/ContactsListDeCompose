b7fdb4e8676c918e219c55e9da1ab812832806cd
Foxxx48 <Foxxdim@mail.ru>
2024-02-28 22:40:32 +0300

    Added dependencies for ViewModel

--- app/src/main/java/com/foxxx/contactslistdecompose/MainActivity.kt
+++ app/src/main/java/com/foxxx/contactslistdecompose/MainActivity.kt
@@ -3,17 +3,10 @@ package com.foxxx.contactslistdecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.foxxx.contactslistdecompose.domain.Contact
 import com.foxxx.contactslistdecompose.ui.contact.AddContact
 import com.foxxx.contactslistdecompose.ui.contact.Contacts
